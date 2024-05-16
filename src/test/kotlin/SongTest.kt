083748fc6fdbc86bba5a034b65fb2fde492099d5
Asif Shaikh <asifs@spotify.com>
2024-05-16 17:43:20 +0200

    Prints stanza no 10

--- src/test/kotlin/SongTest.kt
+++ src/test/kotlin/SongTest.kt
@@ -1,12 +1,18 @@
 import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
 
 class SongTest {
     @Test
     internal fun `should print first stanza`() {
+        // GIVEN
         val stanza = Stanza()
+
+        // WHEN
         val text = stanza.drink(99)
-        Truth.assertThat(text).isEqualTo(
+
+        // THEN
+        assertThat(text).isEqualTo(
             """
                 99 bottles of beer on the wall, 99 bottles of beer.
                 Take one down and pass it around, 98 bottles of beer on the wall.
@@ -17,11 +23,27 @@ class SongTest {
     @Test
     internal fun `should print stanza 86`() {
         val stanza = Stanza()
+
         val text = stanza.drink(86)
-        Truth.assertThat(text).isEqualTo(
+
+        assertThat(text).isEqualTo(
             """
                 86 bottles of beer on the wall, 86 bottles of beer.
                 Take one down and pass it around, 85 bottles of beer on the wall.
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    internal fun `should print stanza number 10`() {
+        val stanza = Stanza()
+
+        val text = stanza.drink(10)
+
+        assertThat(text).isEqualTo(
+            """
+                10 bottles of beer on the wall, 10 bottles of beer.
+                Take one down and pass it around, 9 bottles of beer on the wall.
             """.trimIndent()
         )
     }
